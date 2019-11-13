7399555a4db4a10c71b9282a740846b77b7f1cd3
Shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-11-13 23:00:15 +0600

    fetching data from metaweather

--- src/js/dataCollection.js
+++ src/js/dataCollection.js
@@ -4,7 +4,8 @@ const dataCollertion = (() => {
     const locationUrl = `https://www.metaweather.com/api/location/search/?query=${city}`;
     const weatherFetchUrl = 'https://www.metaweather.com/api/location/';
     const loacationInfo = fetch(proxyUrl + locationUrl).then((data) => data.json());
-    console.log(loacationInfo);
+    const weatherData = await loacationInfo.then((d) => fetch(proxyUrl + weatherFetchUrl + d[0].woeid)).then((d) => d.json()).catch(() => console.log('City not found!'));
+    console.log(weatherData);
   };
   return {
     getData,
