ff94477599ce677974d59d1da4e35bd032ce1ed4
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2020-03-30 17:40:27 +0600

    :art: get current location of a user.
    Fetch current location of a user and asks him to enable share location

--- src/js/dataCollection.js
+++ src/js/dataCollection.js
@@ -1,14 +1,26 @@
 const dataCollertion = (() => {
+  const proxyUrl = 'https://yacdn.org/proxy/';
+  const locationUrl = 'https://www.metaweather.com/api/location/search/?';
   const getData = async (city) => {
-    const proxyUrl = 'https://yacdn.org/proxy/';
-    const locationUrl = `https://www.metaweather.com/api/location/search/?query=${city}`;
     const weatherFetchUrl = 'https://www.metaweather.com/api/location/';
-    const loacationInfo = fetch(proxyUrl + locationUrl).then((data) => data.json());
+    const loacationInfo = fetch(`${proxyUrl + locationUrl}query=${city}`).then((data) => data.json());
     const weatherData = await loacationInfo.then((d) => fetch(proxyUrl + weatherFetchUrl + d[0].woeid)).then((d) => d.json()).catch(() => alert('City not found!'));
     return weatherData;
   };
+  const getCurrentLocation = () => {
+    let cityName;
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        const { latitude, longitude } = position.coords;
+        const currentCity = fetch(`${proxyUrl + locationUrl}lattlong=${latitude},${longitude}`).then((data) => data.json());
+        currentCity.then(data => { cityName = data[0].title; console.log(cityName); });
+      });
+    }
+    return cityName;
+  };
   return {
     getData,
+    getCurrentLocation,
   };
 })();
 export default dataCollertion;
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import '../main.scss';
+import dataCollection from './dataCollection';
 import eventListener from './eventListener';
 
+const currentCity = dataCollection.getCurrentLocation();
 eventListener.enableSearch();
